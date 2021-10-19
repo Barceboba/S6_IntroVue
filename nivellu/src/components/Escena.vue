b70a7f0b5860ae232fec395b7005786a9f9f8a9c
Barceboba <miquel@barceboba.com>
2021-10-19 16:45:43 +0200

    NivellU prest

---
+++ nivellu/src/components/Escena.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+     <!-- - Exercici 1 -->
+     "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial"
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ nivellu/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="home">
+      <Escena></Escena>
+  </div>
+</template>
+
+<script>
+import Escena from '@/components/Escena'
+
+export default {
+    name: "Home",
+    components: {
+        Escena
+    }
+
+}
+</script>
+
+<style>
+
+</style>
