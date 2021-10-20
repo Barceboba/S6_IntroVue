bd7753b948e29366ca469296f46215ef9bf05bdc
Barceboba <miquel@barceboba.com>
2021-10-20 12:49:12 +0200

    Nivell Ú a la saca

--- nivellu/src/components/Escena.vue
+++ nivellu/src/components/Escena.vue
@@ -1,13 +1,27 @@
 <template>
  <div>
      <!-- - Exercici 1 -->
-     "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial."
+     <div>El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial.</div>
+     <div v-for="frase in frases" v-bind:key="frase.id">
+         <div>{{ frase.sentencia }}</div>
+     </div>
  </div>
 </template>
 
 <script>
+import frases from "@/assets/json/historiaSideral.json";
 export default {
-
+name:'Escena',
+props: {
+    msg: String
+},
+computed: {
+    frases() {
+        return frases.map((frase) => {
+            return frase
+        })
+    }
+}
 }
 </script>
 
